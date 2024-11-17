11b930203c48665935c8e6fbfdd876d87482c4d3
srikanthbisai <srikanthbisai2110@gmail.com>
2024-11-17 16:47:04 +0530

    responsive fix

--- components/PairSelector.tsx
+++ components/PairSelector.tsx
@@ -8,7 +8,7 @@ interface PairSelectorProps {
 const PairSelector: React.FC<PairSelectorProps> = ({ selectedPair, setSelectedPair }) => {
   return (
     <div className=" text-black space-x-10 items-center  lg:ml-20 lg:mt-6 flex">
-      <label htmlFor="pair" className="font-serif font-semibold text-3xl text-orange-500 ">
+      <label htmlFor="pair" className="font-serif font-semibold max:md-text-2xl lg:text-3xl text-orange-500 ">
         Select Trading Pair
       </label>
       <select
